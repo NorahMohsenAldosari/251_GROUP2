9209327bb9a8fd54a56e78386326df28baaf884c
Raghad <raghoda2004@icloud.com>
2025-04-22 22:48:17 +0300

    Updated Appointment.java with new logic

--- src/main/java/Appointment.java
+++ src/main/java/Appointment.java
@@ -1,5 +1,54 @@
+
 package main.java;
+import java.util.Random;
 
 public class Appointment {
+    
+ 
+        private int id;
+        private String customerName;
+        private String date;
+        private String time;
+        private String location;
+        private boolean isBooked;
+    
+        public Appointment(String date, String time, String location) {
+            this.id = generateRandomId();
+            this.customerName = "Not booked";
+            this.date = date;
+            this.time = time;
+            this.location = location;
+            this.isBooked = false;
+        }
+    
+        private int generateRandomId() {
+            Random random = new Random();
+            return 1000 + random.nextInt(9000);
+        }
+      
+            public int getId() {
+                return id;
+            }
+        
+            public String getCustomerName() {
+                return customerName;
+            }
+        
+            public String getDate() {
+                return date;
+            }
+        
+            public String getTime() {
+                return time;
+            }
+        
+            public String getLocation() {
+                return location;
+            }
+        
+            public boolean isBooked() {
+                return isBooked;
+            }
+        }
+    
 
-}
