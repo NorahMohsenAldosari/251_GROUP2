9f199469cb1b14ca148eda485d784b575fdc7265
RifalAlsulami <Reefal.alsulami@gmail.com>
2025-04-23 22:45:25 +0300

    update App.java

--- src/App.java
+++ src/App.java
@@ -1,4 +1,60 @@
+import java.util.Random;
+
 public class App {
+
+
+    
+    int id ;
+    String customerName ;
+    String date ;
+    String time ;
+    String location ;
+    boolean isBooked ; 
+    
+    
+    private int generateRandomId() {
+        Random random = new Random();
+        return 1000 + random.nextInt(9000);
+    }
+  
+        public int getId() {
+            return id;
+        }
+    
+        public String getCustomerName() {
+            return customerName;
+        }
+    
+        public String getDate() {
+            return date;
+        }
+    
+        public String getTime() {
+            return time;
+        }
+    
+        public String getLocation() {
+            return location;
+        }
+    
+        public boolean isBooked() {
+            return isBooked;
+        }
+
+    
+
+    public void displayAppointment() {
+        
+        System.out.println("Appointment ID: " + id);
+        System.out.println("Date: " + date);
+        System.out.println("Time: " + time);
+        System.out.println("Location: " + location);
+        
+        System.out.println("Customer: " + (isBooked ? customerName : "Available"));
+
+    }
+
+
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
     }
