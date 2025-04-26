3f1d39a371eb69b994a701ee1a74caf32c238354
Maha Almotairi <160640532+MahaMohammedAlmotairi@users.noreply.github.com>
2025-04-26 03:36:56 +0300

    Add files via upload

--- src/Appointment.java
+++ src/Appointment.java
@@ -1,70 +1,54 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 
-/**
- *
- * @author nora
- */
+package main.java;
 import java.util.Random;
 
 public class Appointment {
-    private int id;
-    private String customerName;
-    private String date;
-    private String time;
-    private String location;
-    private boolean isBooked;
-
-    public Appointment(String date, String time, String location) {
-        this.id = generateRandomId();
-        this.customerName = "Not booked";
-        this.date = date;
-        this.time = time;
-        this.location = location;
-        this.isBooked = false;
-    }
-
-    private int generateRandomId() {
-        Random random = new Random();
-        return 1000 + random.nextInt(9000);
-    }
-     public int getId() {
-        return id;
-    }
-
-    public String getCustomerName() {
-        return customerName;
-    }
-
-    public String getDate() {
-        return date;
-    }
-
-    public String getTime() {
-        return time;
-    }
-
-    public String getLocation() {
-        return location;
-    }
-
-    public boolean isBooked() {
-        return isBooked;
-    }
-}
-public class Appointment {
-    // ... (fields and constructor omitted for brevity)
-
-    public void displayAppointment() {
-        System.out.println("Appointment ID: " + id);
-        System.out.println("Date: " + date);
-        System.out.println("Time: " + time);
-        System.out.println("Location: " + location);
-        System.out.println("Customer: " + (isBooked ? customerName : "Available"));
-        System.out.println("----------------------");
-    }
-}
-}
+    
+ 
+        private int id;
+        private String customerName;
+        private String date;
+        private String time;
+        private String location;
+        private boolean isBooked;
+    
+        public Appointment(String date, String time, String location) {
+            this.id = generateRandomId();
+            this.customerName = "Not booked";
+            this.date = date;
+            this.time = time;
+            this.location = location;
+            this.isBooked = false;
+        }
+    
+        private int generateRandomId() {
+            Random random = new Random();
+            return 1000 + random.nextInt(9000);
+        }
+      
+            public int getId() {
+                return id;
+            }
+        
+            public String getCustomerName() {
+                return customerName;
+            }
+        
+            public String getDate() {
+                return date;
+            }
+        
+            public String getTime() {
+                return time;
+            }
+        
+            public String getLocation() {
+                return location;
+            }
+        
+            public boolean isBooked() {
+                return isBooked;
+            }
+        }
+    
 
